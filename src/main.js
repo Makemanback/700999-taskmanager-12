2fcc0d7eb53ebf42635d2d8821309e28c44da343
makemanback <makemanback@gmail.com>
2020-08-31 22:45:09 +0200

    7-1-6

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
 
 import BoardPresenter from "./presenter/board.js";
+import TasksModel from "./model/tasks.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 const Task = {
@@ -14,13 +15,16 @@ const Task = {
 const tasks = new Array(Task.COUNT).fill(``).map(generateTask);
 const filters = generateFilter(tasks);
 
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
+
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(siteMainElement);
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
 render(siteMainElement, new FilterView(filters), RenderPosition.BEFOREEND);
 
-boardPresenter.init(tasks);
+boardPresenter.init();
